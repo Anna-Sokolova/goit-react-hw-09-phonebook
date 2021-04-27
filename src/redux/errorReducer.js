a86149c34dc19f8dca895c7c0dd412459dfeb232
Anna Sokolova <sokolovaskorik@gmail.com>
2021-04-27 14:17:33 +0200

    Fixed error messange

--- src/redux/errorReducer.js
+++ src/redux/errorReducer.js
@@ -18,11 +18,11 @@ const {
 
 //редьюсер для обработки ошибки
 export const errorReducer = createReducer(null, {
-  [registerAuthError]: (_, action) => console.log(action.payload),
-  [loginAuthError]: (_, action) => console.log(action.payload),
-  [logoutAuthError]: (_, action) => console.log(action.payload),
-  [getCurrentUserError]: (_, action) => console.log(action.payload),
-  [fetchContactError]: (_, action) => console.log(action.payload),
-  [addContactError]: (_, action) => console.log(action.payload),
-  [deleteContactError]: (_, action) => console.log(action.payload),
+  [registerAuthError]: (_, action) => console.error(action.payload),
+  [loginAuthError]: (_, action) => console.error(action.payload),
+  [logoutAuthError]: (_, action) => console.error(action.payload),
+  [getCurrentUserError]: (_, action) => console.error(action.payload),
+  [fetchContactError]: (_, action) => console.error(action.payload),
+  [addContactError]: (_, action) => console.error(action.payload),
+  [deleteContactError]: (_, action) => console.error(action.payload),
 });
