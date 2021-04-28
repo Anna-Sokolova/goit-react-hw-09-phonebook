3ea230114a908e3f046786f027eb657625e4b10e
Anna Sokolova <sokolovaskorik@gmail.com>
2021-04-28 13:21:14 +0200

    Changed the ContactList to hooks

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,51 +1,40 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
 import operationsContact from '../../redux/contacts/contacts-operations';
 import selectorsContact from '../../redux/contacts/contacts-selectors';
 import styles from './ContactList.module.css';
 
-const ContactList = ({ contacts, onDeleteContact }) => (
-  <ul className={styles.list}>
-    {contacts &&
-      contacts.map(({ id, name, number }) => (
-        <li key={id} className={styles.item}>
-          <p>
-            {name}
-            <span className={styles.divider}>:</span>
-          </p>
-          <p>
-            <span className={styles.tel}>{number}</span>
-          </p>
-          <button
-            type="button"
-            className={styles.btnDelete}
-            onClick={() => onDeleteContact(id)}
-          >
-            Delete
-          </button>
-        </li>
-      ))}
-  </ul>
-);
+export default function ContactList() {
+  //useSelectors as mapStateToProps
+  const contacts = useSelector(state =>
+    selectorsContact.getFilteredByName(state),
+  );
 
-ContactList.propTypes = {
-  contacts: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      number: PropTypes.string.isRequired,
-    }),
-  ).isRequired,
-  onDeleteContact: PropTypes.func.isRequired,
-};
+  //useDispatch as mapDispatchToProps
+  const dispatch = useDispatch();
+  const onDeleteContact = id => dispatch(operationsContact.deleteContact(id));
 
-const mapStateToProps = state => ({
-  contacts: selectorsContact.getFilteredByName(state),
-});
-
-const mapDispatchToProps = dispatch => ({
-  onDeleteContact: id => dispatch(operationsContact.deleteContact(id)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(ContactList);
+  return (
+    <ul className={styles.list}>
+      {contacts &&
+        contacts.map(({ id, name, number }) => (
+          <li key={id} className={styles.item}>
+            <p>
+              {name}
+              <span className={styles.divider}>:</span>
+            </p>
+            <p>
+              <span className={styles.tel}>{number}</span>
+            </p>
+            <button
+              type="button"
+              className={styles.btnDelete}
+              onClick={() => onDeleteContact(id)}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+    </ul>
+  );
+}
