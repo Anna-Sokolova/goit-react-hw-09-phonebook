9429e24e90c969e07f4ce2611868d4c713c0bbd7
Anna Sokolova <sokolovaskorik@gmail.com>
2021-04-28 17:45:20 +0200

    Changed the RegisterForm to hooks

--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -1,120 +1,123 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { useState, useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Title from '../../components/Title';
 import Spinner from '../../components/Spinner';
 import operationsAuth from '../../redux/auth/auth-operations';
 import selectorsAuth from '../../redux/auth/auth-selectors';
 import styles from './RegisterForm.module.css';
 
-class RegisterForm extends Component {
-  state = {
-    name: '',
-    email: '',
-    password: '',
-  };
+export default function RegisterForm() {
+  //useState
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  handleInputChange = e => {
+  //useSelector as mapStateToProps
+  const isLoadingUser = useSelector(state => selectorsAuth.getLoading(state));
+
+  //useDispatch as mapDispatchToProps
+  const dispatch = useDispatch();
+
+  const handleInputChange = useCallback(e => {
     const { name, value } = e.target;
-    this.setState({ [name]: value });
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+
+      case 'email':
+        setEmail(value);
+        break;
+
+      case 'password':
+        setPassword(value);
+        break;
+
+      default:
+        console.warn(`Такого поля ${name} не существует`);
+    }
+  }, []);
+
+  const handleSubmit = useCallback(
+    e => {
+      e.preventDefault();
+
+      // dispatch как замена this.props.onRegister(this.state);
+      dispatch(operationsAuth.register({ name, email, password }));
+      reset();
+
+      alert(`
+      Спасибо за регистрацию!
+      Логин: ${name}
+      Почта: ${email}
+      Пароль: ${password}
+    `);
+    },
+    [name, email, password, dispatch],
+  );
+
+  const reset = () => {
+    setName('');
+    setEmail('');
+    setPassword('');
   };
 
-  handleSubmit = e => {
-    e.preventDefault();
+  return (
+    <>
+      {isLoadingUser && <Spinner />}
+      <Title title="Регистрация пользователя" />
+      <div className={styles.registerForm}>
+        <fieldset>
+          <form
+            onSubmit={handleSubmit}
+            className={styles.form}
+            autoComplete="off"
+          >
+            <label className={styles.label}>
+              Имя
+              <input
+                className={styles.formInput}
+                type="text"
+                name="name"
+                placeholder="Enter name"
+                value={name}
+                onChange={handleInputChange}
+                required
+              />
+            </label>
 
-    this.props.onRegister(this.state);
+            <label className={styles.label}>
+              Почта
+              <input
+                className={styles.formInput}
+                type="email"
+                name="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={handleInputChange}
+                required
+              />
+            </label>
 
-    this.reset();
+            <label className={styles.label}>
+              Пароль
+              <input
+                className={styles.formInput}
+                type="password"
+                name="password"
+                placeholder="Enter password"
+                value={password}
+                onChange={handleInputChange}
+                required
+              />
+            </label>
 
-    alert(`
-    Спасибо за регистрацию!
-    Логин: ${this.state.name}
-    Почта: ${this.state.email}
-  `);
-  };
-
-  reset = () => {
-    this.setState({
-      name: '',
-      email: '',
-      password: '',
-    });
-  };
-
-  render() {
-    const { name, email, password } = this.state;
-
-    return (
-      <>
-        {this.props.isLoadingUser && <Spinner />}
-        <Title title="Регистрация пользователя" />
-        <div className={styles.registerForm}>
-          <fieldset>
-            <form
-              onSubmit={this.handleSubmit}
-              className={styles.form}
-              autoComplete="off"
-            >
-              <label className={styles.label}>
-                Имя
-                <input
-                  className={styles.formInput}
-                  type="text"
-                  name="name"
-                  placeholder="Enter name"
-                  value={name}
-                  onChange={this.handleInputChange}
-                  required
-                />
-              </label>
-
-              <label className={styles.label}>
-                Почта
-                <input
-                  className={styles.formInput}
-                  type="email"
-                  name="email"
-                  placeholder="Enter email"
-                  value={email}
-                  onChange={this.handleInputChange}
-                  required
-                />
-              </label>
-
-              <label className={styles.label}>
-                Пароль
-                <input
-                  className={styles.formInput}
-                  type="password"
-                  name="password"
-                  placeholder="Enter password"
-                  value={password}
-                  onChange={this.handleInputChange}
-                  required
-                />
-              </label>
-
-              <button type="submit" className={styles.btnSubmit}>
-                Зарегистрироваться
-              </button>
-            </form>
-          </fieldset>
-        </div>
-      </>
-    );
-  }
+            <button type="submit" className={styles.btnSubmit}>
+              Зарегистрироваться
+            </button>
+          </form>
+        </fieldset>
+      </div>
+    </>
+  );
 }
-
-const mapStateToProps = state => ({
-  isLoadingUser: selectorsAuth.getLoading(state),
-});
-
-const mapDispatchToProps = dispatch => ({
-  onRegister: data => dispatch(operationsAuth.register(data)),
-});
-
-//сокращенная запись как вариант
-// const mapDispatchToProps = {
-//   onRegister: dispatch(operationsAuth.register),
-// };
-
-export default connect(mapStateToProps, mapDispatchToProps)(RegisterForm);
